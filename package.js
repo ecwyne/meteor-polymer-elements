748b856e20e833bdc7bf065484d63317539d8106
Eric Wyne <ecwyne@gmail.com>
2014-09-17 12:17:02 -0700

    added underscore

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "Add Polymer-Elements to Meteor",
-	version: "1.0.0",
+	version: "1.0.1",
 	git: "http://github.com/ecwyne/meteor-polymer-elements",
 	name: 'ecwyne:polymer-elements'
 });
@@ -10,7 +10,8 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-	api.use('ecwyne:polymer');
+	api.use('ecwyne:polymer@1.0.1');
+	api.use('underscore');
 	api.versionsFrom('METEOR@0.9.0');
 	api.addFiles('polymer-elements.js');
 });
