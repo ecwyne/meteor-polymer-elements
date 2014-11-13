ed700139b618742d93efe5acbb9c471ff23db4f5
Eric Wyne <ecwyne@gmail.com>
2014-11-13 08:31:40 -0800

    update to 0.5.0

--- package.js
+++ package.js
@@ -1,16 +1,16 @@
 Package.describe({
 	summary: "Add Polymer-Elements to Meteor",
-	version: "1.0.3",
+	version: "1.0.4",
 	git: "http://github.com/ecwyne/meteor-polymer-elements",
 	name: 'ecwyne:polymer-elements'
 });
 
 Npm.depends({
-	bower: '1.3.10'
+	bower: '1.3.12'
 });
 
 Package.onUse(function(api) {
-	api.use('ecwyne:polymer@1.0.2');
+	api.use('ecwyne:polymer@1.0.3');
 	api.use('underscore');
 	api.versionsFrom('METEOR@0.9.0');
 	api.addFiles('polymer-elements.js', 'server');
