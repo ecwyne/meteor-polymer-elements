4b6ac34f8f5adf3d69067c918824adf42754bbcb
Hardik Joshi <mail@hardikjoshi.me>
2015-01-03 02:50:12 +0530

    upgraded polymer to 0.5.2

--- polymer-elements.js
+++ polymer-elements.js
@@ -22,6 +22,6 @@ _.forEach(bowerCommands, function (command) {
 	var localCache = _.values(Bower.list(null, {offline: true, directory: dir}).pkgMeta.dependencies);
 	if (!_.contains(localCache, 'Polymer/core-elements#0.5.0')){
 		console.log('installing polymer-elements');
-		Bower.install(['Polymer/core-elements#0.5.0', 'Polymer/paper-elements'], {save: true}, {directory: dir});
+		Bower.install(['Polymer/core-elements#0.5.2', 'Polymer/paper-elements'], {save: true}, {directory: dir});
 	}
 }
